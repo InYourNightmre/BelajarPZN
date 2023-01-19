0732393432a28624fa24197946152e1a38ec375f
InYourNightmre <smpcgaa@gmail.com>
2023-01-19 18:18:49 +0700

    first commit

---
+++ script JS/HelloWorldFIle.js
@@ -0,0 +1,10 @@
+document.writeln('Hello World')
+// komentar satu baris
+document.writeln("<p>Hello World</p>");
+
+/*
+komentar ini bisa lebih dari satu baris
+seperti ini
+sampa ketemu tanda
+ini
+*/
